693f9f63fad14435649890761dde3a7530f5712c
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2019-06-28 13:12:52 +0100

    Update jenkins_slave.tf

--- jenkins_slave.tf
+++ jenkins_slave.tf
@@ -4,6 +4,18 @@ resource "azurerm_network_security_group" "second" {
 	resource_group_name = "${azurerm_resource_group.main.name}"
 
 	security_rule {
+		name = "SSH"
+        	priority = 100
+        	direction = "Inbound"
+        	access = "Allow"
+        	protocol = "Tcp"
+        	source_port_range = "*" 
+        	destination_port_range = "22"
+        	source_address_prefix = "*"
+        	destination_address_prefix = "*"
+	}
+	
+	security_rule {
                 name = "HTTPS"
                 priority = 150
                 direction = "Inbound"
