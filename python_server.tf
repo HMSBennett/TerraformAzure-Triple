f2b3863f47c350060104c2b0fbb1888f200ace05
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2019-06-28 13:13:01 +0100

    Update python_server.tf

--- python_server.tf
+++ python_server.tf
@@ -4,6 +4,18 @@ resource "azurerm_network_security_group" "third" {
 	resource_group_name = "${azurerm_resource_group.main.name}"
 
 	security_rule {
+		name = "SSH"
+        	priority = 100
+        	direction = "Inbound"
+        	access = "Allow"
+        	protocol = "Tcp"
+        	source_port_range = "*" 
+        	destination_port_range = "22"
+        	source_address_prefix = "*"
+        	destination_address_prefix = "*"
+	}
+	
+	security_rule {
                 name = "Server"
                 priority = 200
                 direction = "Inbound"
